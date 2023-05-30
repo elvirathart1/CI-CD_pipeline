7690d2d7da6737e42fe0294c499af6a03b6debf7
elvirathart <elvirathart@valori.nl>
2023-05-30 19:36:08 +0200

    change in code

--- TypeScript/pages/home-page.ts
+++ TypeScript/pages/home-page.ts
@@ -3,7 +3,7 @@ import { expect, Locator, Page } from '@playwright/test';
 export class HomePage {
     readonly page: Page;
     readonly signInButton: Locator;
-
+////
     constructor(page: Page) {
         this.page = page;
         this.signInButton = page.locator('');
